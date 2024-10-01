34af64ec4e8bcbc097f53c23b3a4ecb07c9a8f69
bgovha <brendagovha1@gmail.com>
2024-10-01 14:15:20 +0200

    modified the html by making the interface user friendly

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ function generatePoem(event) {
   let instructions= document.querySelector("#instructions")
 let apiKey = "2abt3d4e02aa5e6116ef006dbocad3a0";
 let prompt=`generate a poem about ${instructions.value}`;
-let context="you are a romantic poem expert and love to write short poems. your mission is to write 4 ine poems in basic htm, make sure to folow the user instructions";
+let context="you are a romantic poem expert and love to write short poems. your mission is to write 4 line poems, make sure to folow the user instructions";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML= `<div class = "generating">⌛generating a poem...</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 
